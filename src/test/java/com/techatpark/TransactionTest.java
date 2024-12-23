b118ecc3d24896a103dda152d723607ddc3b578f
sathishk <sathishkumar.thiyagarajan@gmail.com>
2024-12-23 20:17:04 +0530

    Trannsaction Testcases Added

--- src/test/java/com/techatpark/TransactionTest.java
+++ src/test/java/com/techatpark/TransactionTest.java
@@ -5,12 +5,31 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TransactionTest extends BaseTest {
 
     @Test
     void testTransaction() throws SQLException {
+
+        Transaction
+            .begin(new SqlBuilder("INSERT INTO movie ( title ,directed_by ) VALUES ( ? ,? )")
+                    .param("Inception")
+                    .param("Christopher Nolan"))
+            // Invalid Insert (title can not be null). Should Fail.
+            .thenApply(value -> new SqlBuilder("INSERT INTO movie ( title ,directed_by ) VALUES ( ? ,? )")
+                    .param("Dunkrik")
+                    .param("Christopher Nolan"))
+            .commit(dataSource);
+
+
+        Assertions.assertEquals(2,
+                new SqlBuilder("SELECT id, title, directed_by from movie")
+                        .queryForList(BaseTest::mapMovie)
+                        .execute(dataSource).size());
+
+    }
+
+    @Test
+    void testInvalidTransaction() throws SQLException {
 
         Assertions.assertThrows(SQLException.class, () -> {
             Transaction
